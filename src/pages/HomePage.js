618ab6079dca7431e831939504647a297e1f1eb7
Emmanuel <mailkumah@gmail.com>
2022-05-24 11:44:18 +0000

    Add Navigation menu

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "./Navigation/Navigation";
+
+const HomePage = () => {
+  return (
+    <>
+      <header>
+        <Navigation />
+      </header>
+      <main>{/* <h2>Main content goes hers</h2> */}</main>
+      <footer>{/* <h2>Footer content goes here</h2> */}</footer>
+    </>
+  );
+};
+
+export default HomePage;
