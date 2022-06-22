d34a01406c46b87401e292ed2c243bd0e05c8ace
Emmanuel <mailkumah@gmail.com>
2022-06-22 11:21:36 +0000

    Add responsive navbar to NavBar.module.css

--- src/pages/Home/Sections/NavBar.js
+++ src/pages/Home/Sections/NavBar.js
@@ -2,65 +2,75 @@ import React, { useState, useEffect } from "react";
 import classes from "./NavBar.module.css";
 import { FaTimes, FaBars } from "react-icons/fa";
 import logo from "./image/okadaLogo.png";
-import { NavLink, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const NavBar = () => {
-  const [isSticky, setIsSticky] = useState(false);
+  // const [isSticky, setIsSticky] = useState(false);
   const [isMenuClicked, setsIsMenuClicked] = useState(false);
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleStickyNav);
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleStickyNav);
+  // }, []);
 
   const handleMenuIconClick = () => {
     setsIsMenuClicked(!isMenuClicked);
   };
 
-  const handleStickyNav = () => {
-    if (window !== undefined) {
-      let windowHeight = window.scrollY;
-      windowHeight > 150 ? setIsSticky(true) : setIsSticky(false);
-    }
-  };
+  // const handleStickyNav = () => {
+  //   if (window !== undefined) {
+  //     let windowHeight = window.scrollY;
+  //     windowHeight > 150 ? setIsSticky(true) : setIsSticky(false);
+  //   }
+  // };
 
   return (
     <>
-      <header
-        className={`${classes.navContainer} ${isSticky ? classes.sticky : ""}`}
-      >
-        <div className={classes.navBrand}>
-          <h1>
-            <span>
-              <img src={logo} alt="okadaRide" className={classes.navLogo} />
-            </span>
-            Okada Ride
-          </h1>
-        </div>
-
-        <div className={classes.navMenu}>
-          <div className={classes["nm--icon"]} onClick={handleMenuIconClick}>
-            {isMenuClicked ? <FaTimes /> : <FaBars />}
+      <header className={classes.navBar}>
+        <div className={classes["navBar--container"]}>
+          <div className={classes["navBrand--container"]}>
+            <Link to="/" className={classes.navBrand}>
+              <h1>
+                <span>
+                  <img src={logo} alt="okadaRide" className={classes.navLogo} />
+                </span>
+                Okada Ride
+              </h1>
+            </Link>
+            <div
+              className={classes["navMenu--icon"]}
+              onClick={handleMenuIconClick}
+            >
+              {isMenuClicked ? <FaTimes /> : <FaBars />}
+            </div>
           </div>
           <nav>
-            <ul>
+            <ul
+              className={`${
+                isMenuClicked ? classes["navMenu--active"] : classes.navMenu
+              }`}
+            >
               <li>
-                <NavLink activeclassname={classes.active} to="/">
+                <NavLink className={classes["navMenu--item"]} to="/">
                   Home
                 </NavLink>
               </li>
               <li>
-                <NavLink to="#about">About</NavLink>
+                <NavLink className={classes["navMenu--item"]} to="#about">
+                  About
+                </NavLink>
               </li>
               <li>
-                <NavLink to="#FAQ">FAQs</NavLink>
+                <NavLink className={classes["navMenu--item"]} to="#FAQ">
+                  FAQs
+                </NavLink>
               </li>
               <li>
-                <NavLink activeclassname={classes.active} to="/motors">
+                <NavLink className={classes["navMenu--item"]} to="/motors">
                   Find Motors
                 </NavLink>
               </li>
               <li>
-                <NavLink activeclassname={classes.active} to="/tools">
+                <NavLink className={classes["navMenu--item"]} to="/tools">
                   Fuel Calculator
                 </NavLink>
               </li>
